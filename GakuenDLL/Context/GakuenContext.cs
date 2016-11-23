d4752c742ea3cd97a68a718306f50151f0f7f128
MartinDataStuff <martin1-g@hotmail.com>
2016-11-23 14:32:07 +0100

    Added a large chunk of the entities, made a connection to the database

---
+++ GakuenDLL/Context/GakuenContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GakuenDLL.Context
+{
+    class GakuenContext : DbContext
+    {
+        public GakuenContext() : base("name=Gakuen")
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<GakuenContext>());
+        }
+    }
+}

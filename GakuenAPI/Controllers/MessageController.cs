08e4ff9edb1ea772ea746e0e3a12ff43f8cf6048
MartinDataStuff <martin1-g@hotmail.com>
2016-11-24 14:54:53 +0100

    Working on Email Verifacation

---
+++ GakuenAPI/Controllers/MessageController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net.Mail;
+using System.Text;
+using GakuenAPI.Models;
+
+namespace GakuenAPI.Controllers
+{
+    public class MessageController : ApiController
+    {
+        // POST: api/EventMessages
+        public IHttpActionResult PostMessage(Message message)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            MailMessage email = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+
+            // set up the Gmail server
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+            smtp.Credentials = new System.Net.NetworkCredential(Message.Sender, Message.Code);
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false;
+
+            // draft the email
+            MailAddress fromAddress = new MailAddress(Message.Sender);
+            email.From = fromAddress;
+            email.To.Add(message.Reciever);
+            email.Subject = message.Header;
+            email.Body = message.Body;
+
+            smtp.Send(email);
+
+
+
+            return CreatedAtRoute("DefaultApi", new { id = message.Id }, message);
+        }
+    }
+}
---
+++ GakuenAPI/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GakuenAPI.Models
+{
+    public class Message
+    {
+
+
+        public int Id { get; set; }
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public string Reciever { get; set; }
+        public const string Sender = "gakuenreply@gmail.com";
+        public const string Code = "p@ssW0rd";
+    }
+}
